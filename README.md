c6374745b9013277a1376553c24b1bd2a65231c4
hala277 <hala.almasharfeh@gmail.com>
2021-08-30 01:48:56 +0300

    add js

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # jsPractice
+1. **add alert**
+2. **add while loop and if else to the alert**
+3. **use switch**
+4. **array**
+5. **add user name function**
+6. **add obj**
---
+++ app.js
@@ -0,0 +1,131 @@
+
+'use strict';
+// alert 
+
+// let userName2 = prompt('please enter your name');
+// document.write(`<h2>${userName2}</h2>`);
+// alert("oh Hi nice to met you  "+ userName2);
+// console.log(userName2);
+
+// add while loop and if else to the alert
+// let readBook ;
+
+// while ( readBook != 'yes' || readBook != 'no'){
+//     readBook = prompt('did you read books?')
+// if(readBook == 'yes'){
+//     console.log('good')
+//     alert('good');
+//     break;
+// }
+// else if (readBook == 'no'){
+//     console.log('you shold start read!!')
+//     alert('you shold start read!!');
+//     break;
+// }
+
+// else {
+//     alert('you have to enter yes , no ')
+// }
+// }
+
+// use switch
+// let coffieType = prompt('please enter your coffie type here :  we only have black , late')
+// if (coffieType !== null) {
+//     // coffieType = coffieType.toLowerCase();
+//     coffieType = coffieType.toUpperCase();
+
+// }
+// switch(coffieType){
+//     // case 'black':
+//         case 'BLACK':
+//         alert('nice');
+//         break;
+//     // case 'late':
+//     case 'LATE':
+//     alert('oh waw');
+//     break;
+//     default:
+//         alert('we dont have this one');
+// }
+
+// array
+// let mixArray = ['bmw', 1993, 5000, 'shihab', 'lightblue', true, ['ahmad', 'sami', 'leen'],['hi',['buy']]];
+
+// console.log(mixArray);
+// alert(mixArray);
+// console.log(mixArray[6][2]);
+// console.log(mixArray[3]);
+// console.log(mixArray[7][1][0]);
+
+//function 1
+// add user name function
+// let userName = function (username) {
+//     username = username +' welcome home';
+
+//     return username;
+// }
+
+// let user=prompt('please enter your name');
+// console.log(userName(user));
+// alert('thank you and welcome home' +userName(user));
+// document.write(`<h3>${userName(user)}</h3>`);
+
+//function 2
+// (function (username) {
+//     username = username +' welcome home';
+//     console.log(username);
+//         return username;
+       
+// })('hala');
+
+//function 3
+// function userName(username){
+//     username = username +' welcome home';
+
+//      return username;
+// }
+// let user=prompt('please enter your name');
+// console.log(userName(user));
+// alert('thank you and welcome home' +userName(user));
+// document.write(`<h3>${userName(user)}</h3>`);
+
+// add obj
+// let hala = {
+//     userName : 'hala',
+//     age : 21,
+//     study : 'Software',
+
+//     username:function(){
+       
+//         // console.log(`welcome home ${hala.userName}`);
+//         document.write(`<h3>${hala.userName} welcome home</h3>`);
+//         console.log(`${hala.userName}  welcome home`);
+
+           
+//     }
+// }
+// console.log(hala);
+// hala.username();
+
+const car = {
+    carModel: 'kia'
+}
+
+// here we can Change the properties of constant object or array
+// console.log(car);
+// car.carModel = 'bmw';
+// // car = 'ford';
+// car.price = 5000;
+// console.log(car);
+
+// this one will give me error 
+
+// car ={
+//     carModel: 'ford'
+// }
+// console.log(car);
+
+
+
+
+
